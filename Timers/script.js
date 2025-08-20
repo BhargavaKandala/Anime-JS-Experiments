bdb785c7032f2fd51a5bff702b21cab6bde48ddf
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2025-08-20 15:36:52 +0530

    Timer in animeJS

--- Timers/script.js
+++ Timers/script.js
@@ -1,18 +1,18 @@
-// // no import needed
-// anime({
-//   targets: '.value',
-//   innerHTML: [0, 100],
-//   round: 1,
-//   duration: 2000,
-//   easing: 'linear'
-// });
+// no import needed
+anime({
+  targets: '.value',
+  innerHTML: [0, 100],
+  round: 1,
+  duration: 2000,
+  easing: 'linear'
+});
 
-// const [ $time, $count ] = utils.$('.value');
+const [ $time, $count ] = utils.$('.value');
 
-// createTimer({
-//   duration: 1000,
-//   loop: true,
-//   frameRate: 30,
-//   onUpdate: self => $time.innerHTML = self.currentTime,
-//   onLoop: self => $count.innerHTML = self._currentIteration
-// });
+createTimer({
+  duration: 1000,
+  loop: true,
+  frameRate: 30,
+  onUpdate: self => $time.innerHTML = self.currentTime,
+  onLoop: self => $count.innerHTML = self._currentIteration
+});
