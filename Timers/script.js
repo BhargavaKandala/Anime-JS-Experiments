5fe859ac6106618bdd99420494ee0f2e19d74e2e
BhargavaKandala <144446705+BhargavaKandala@users.noreply.github.com>
2025-08-27 23:57:21 +0530

    timer

--- Timers/script.js
+++ Timers/script.js
@@ -7,12 +7,21 @@ anime({
   easing: 'linear'
 });
 
-const [ $time, $count ] = document.querySelectorAll('.value');
-
 createTimer({
   duration: 1000,
-  loop: true,
-  frameRate: 30,
-  onUpdate: self => $time.innerHTML = self.currentTime,
-  onLoop: self => $count.innerHTML = self._currentIteration
+  frameRate: true, 
+  loop: true,       
+  onBegin: () => {},
+  onLoop: () => {},
+  onUpdate: () => {},
 });
+
+const [ $time, $count ] = document.querySelectorAll('.value');
+
+// createTimer({
+//   duration: 1000,
+//   loop: true,
+//   frameRate: 30,
+//   onUpdate: self => $time.innerHTML = self.currentTime,
+//   onLoop: self => $count.innerHTML = self._currentIteration
+// });
